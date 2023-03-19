babf275806fc16ac2a947b5ba10b140c6324e9f5
aji <ajizkai1301@gmail.com>
2023-03-19 20:18:12 +0700

    add new file async 3

---
+++ asyncio-3.py
@@ -0,0 +1,19 @@
+import asyncio
+
+async def coroutine():
+    print('Start')
+    await asyncio.sleep(1)
+    return 'Done'
+
+async def main():
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(coroutine())
+    result = await task
+    print(result)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
+
